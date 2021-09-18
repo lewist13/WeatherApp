6e703b932cb70aeef1fe7f80342bdf2f6419c02c
lewist13 <lewistpro@gmail.com>
2021-09-18 17:07:57 -0400

    reconfigured datafunctions for deployment

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,59 +1,62 @@
-const WEATHER_API_KEY =  "bc5f2af57e7725a454f3c68aee7462da"
+import fetch from "node-fetch"
 
-export const setLocationObject = (locationObject, coordsObj) => {
-  const {lat, lon, name, unit} = coordsObj;
-  locationObject.setLat(lat)
-  locationObject.setLon(lon)
-  locationObject.setName(name)
+export const setLocationObject = (locationObj, coordsObj) => {
+  const { lat, lon, name, unit } = coordsObj;
+  locationObj.setLat(lat);
+  locationObj.setLon(lon);
+  locationObj.setName(name);
   if (unit) {
-    locationObject.setUnit(unit)
+    locationObj.setUnit(unit);
   }
-}
+};
 
 export const getHomeLocation = () => {
-  return localStorage.getItem("defaultWeatherLocation")
-}
-
-// export const getWeatherFromCoords = async (locationObj) => {
-//   const lat = locationObj.getLat();
-//   const lon = locationObj.getLon();
-//   const units = locationObj.getUnit();
-//   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
-//   try {
-//     const weatherStream = await fetch(url);
-//     const weatherJson = await weatherStream.json();
-//     return weatherJson;
-//   } catch (err) {
-//     console.error(err);
-//   }
-const urlDataObj = {
-  lat: locationObj.getLat(),
-  lon: locationObj.getLon(),
-  units: locationObj.getUnit()
+  return localStorage.getItem("defaultWeatherLocation");
 };
-try {
-  const weatherStream = await fetch("./.netlify/functions/get_weather", {
-    method: "POST",
-    body: JSON.stringify(urlDataObj)
-  });
-  const weatherJson = await weatherStream.json();
-  return weatherJson;
-} catch (err) {
-  console.error(err); 
-}
+
+export const getWeatherFromCoords = async (locationObj) => {
+  /* const lat = locationObj.getLat();
+  const lon = locationObj.getLon();
+  const units = locationObj.getUnit();
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  try {
+    const weatherStream = await fetch(url);
+    const weatherJson = await weatherStream.json();
+    return weatherJson;
+  } catch (err) {
+    console.error(err);
+  } */
+
+  const urlDataObj = {
+    lat: locationObj.getLat(),
+    lon: locationObj.getLon(),
+    units: locationObj.getUnit()
+  };
+  try {
+    const weatherStream = await fetch("./.netlify/functions/get_weather", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj)
+    });
+    const weatherJson = await weatherStream.json();
+    return weatherJson;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const getCoordsFromApi = async (entryText, units) => {
-  // const regex = /^\d+$/g;
-  // const flag = regex.test(entryText) ? "zip" : "q";
-  // const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
-  // const encodedUrl = encodeURI(url);
-  // try {
-  //   const dataStream = await fetch(encodedUrl);
-  //   const jsonData = await dataStream.json();
-  //   return jsonData;
-  // } catch (err) {
-  //   console.error(err.stack);
-  // }
+  /* const regex = /^\d+$/g;
+  const flag = regex.test(entryText) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const encodedUrl = encodeURI(url);
+  try {
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
+    return jsonData;
+  } catch (err) {
+    console.error(err.stack);
+  } */
+
   const urlDataObj = {
     text: entryText,
     units: units
@@ -67,10 +70,11 @@ export const getCoordsFromApi = async (entryText, units) => {
     return jsonData;
   } catch (err) {
     console.error(err);
-  } 
-}
+  }
+};
+
 export const cleanText = (text) => {
-  const regex = / {2,}/g
-  const entryText = text.replaceAll(regex, " ").trim()
-  return entryText
-}
+  const regex = / {2,}/g;
+  const entryText = text.replaceAll(regex, " ").trim();
+  return entryText;
+};
